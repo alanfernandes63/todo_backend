38406b00d554b1f2e7dd430f47eaf2a0a603aa2a
alan <alan.fernandes63@gmail.com>
2020-03-21 13:34:18 -0300

    Remove dependencia spring mvc

--- src/main/java/com/alanfernandes/todos/TodosApplication.java
+++ src/main/java/com/alanfernandes/todos/TodosApplication.java
@@ -19,9 +19,9 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class TodosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodosApplication.class, args);
+    }
 	
     @SuppressWarnings({"rawtypes", "unchecked"})
 	@Bean
