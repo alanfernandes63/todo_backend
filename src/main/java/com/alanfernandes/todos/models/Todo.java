3bdf79f10b01bbefbbada082757b918cb0358d19
alan <alan.fernandes63@gmail.com>
2020-03-22 08:34:15 -0300

    Adiciona anotacoes de dados que nao podem ser nulos

--- src/main/java/com/alanfernandes/todos/models/Todo.java
+++ src/main/java/com/alanfernandes/todos/models/Todo.java
@@ -4,14 +4,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Todo {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@NotNull
 	private long id;
+    
+	@NotNull
 	private String name;
+	@NotNull
 	private boolean done;
 	
 	public Todo() {
